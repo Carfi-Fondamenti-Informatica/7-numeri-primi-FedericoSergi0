18b71699e40c4f27d1f9898be12f61c14195bfc2
FedericoSergi0 <91880033+FedericoSergi0@users.noreply.github.com>
2021-12-01 19:27:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
 
+using namespace std;
 int main(){
+  int a=0;
+  cin >> a;
+  
+  numeriprimi (a);
+  
+  if ( numeriprimi (a)==1){
+    cout << "numero non primo";
+  }
   return 0;
 }
