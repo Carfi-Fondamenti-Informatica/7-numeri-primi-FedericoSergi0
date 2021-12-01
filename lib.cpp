c5757a84042006e41b1beca941a9f8694cf0b350
FedericoSergi0 <91880033+FedericoSergi0@users.noreply.github.com>
2021-12-01 21:44:05 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,20 +1,20 @@
 #include "lib.h"
 
-bool numeriprimi (int a){
+bool numeriprimi ( int a ){
     bool vero_o_falso;
 
-    if (a>2){
-        for (long b=2;b<a;b++){
-            if ((a%b)==0) {
+    if ( a>2){
+        for ( long b=2; b<a ; b++){
+            if ( ( a % b) == 0) {
                 vero_o_falso = false;
                 break;
             }else {
                 vero_o_falso = true;
             }
-            }
-        }else if (a==2) {
+           }
+        } else if (a==2){
         vero_o_falso = true;
-    }else {
+    } else {
         vero_o_falso = false;
     }
         return vero_o_falso;
