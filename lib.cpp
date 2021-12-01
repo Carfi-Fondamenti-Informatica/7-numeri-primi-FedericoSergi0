f4a9a66872fa0da0341cc2987c09ddac4679d5d1
FedericoSergi0 <91880033+FedericoSergi0@users.noreply.github.com>
2021-12-01 19:24:46 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
+bool numeriprimi ( int a)
+int b=2;
+    for (int b=2; b<a; b++);
+int risultato=0;
+    {
+        risultato=(a%b);
+        if(risultato==0)
+      {
+            return false;
+    }
+        else{
+            return true;
+        }
+
+    }
+
 
